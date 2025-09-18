e1d98d1f757eaff932fc21ecb72ea00f8817b3ef
Harshof16 <harshkla09@gmail.com>
2025-09-18 17:41:41 +0530

    statusbar

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,10 +1,14 @@
 import { Stack } from "expo-router";
+import { StatusBar } from "expo-status-bar";
 
 export default function RootLayout() {
   return (
+    <>
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
     </Stack>
+     <StatusBar style="light" />
+    </>
   );
 }
 // A stack navigator is the foundation for navigating between different screens in an app.
